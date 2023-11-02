d91dd4c5578f88fabc666df23ca44182a8660976
rafaelsgomes <rafaelsgomes.dev@gmail.com>
2023-11-02 17:18:00 -0300

    Feat: First commit

---
+++ src/domain/useCases/answerQuestion.ts
@@ -0,0 +1,10 @@
+interface AnswerQuestionUseCaseRequest {
+  instructorId: string
+  questionId: string
+}
+
+export class AnswerQuestionUseCase {
+  execute({instructorId, questionId}: AnswerQuestionUseCaseRequest){
+
+  }
+}
